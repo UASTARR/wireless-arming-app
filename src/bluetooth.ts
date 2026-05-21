586c6faffea460a33eec060a5a82bf39230391a8
Sovereign Shahid <sovereignshahid@gmail.com>
2026-05-20 18:38:23 -0600

    feat: added boilerplate code from docs

--- src/bluetooth.ts
+++ src/bluetooth.ts
@@ -1,3 +1,60 @@
-export function Connect() {
-  console.log("TODO: Connect Device")
+import { BleManager } from 'react-native-ble-plx'
+import { Platform, PermissionsAndroid } from 'react-native';
+import { utils } from './';
+
+// Coppied from https://dotintent.github.io/react-native-ble-plx/#getting-started
+let requestBluetoothPermission = async () => {
+  if (Platform.OS === 'ios') {
+    return true;
+  }
+  if (Platform.OS === 'android' && PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION) {
+    const apiLevel = parseInt(Platform.Version.toString(), 10);
+
+    if (apiLevel < 31) {
+      const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+      return granted === PermissionsAndroid.RESULTS.GRANTED;
+    }
+    if (PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN && PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT) {
+      const result = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      ]);
+
+      return (
+        result['android.permission.BLUETOOTH_CONNECT'] === PermissionsAndroid.RESULTS.GRANTED &&
+        result['android.permission.BLUETOOTH_SCAN'] === PermissionsAndroid.RESULTS.GRANTED &&
+        result['android.permission.ACCESS_FINE_LOCATION'] === PermissionsAndroid.RESULTS.GRANTED
+      );
+    }
+  }
+
+  utils.showAlert('Permission have not been granted');
+
+  return false;
 }
+
+// Immediately call this once module is loaded to get bluetooth permissions on app load
+requestBluetoothPermission();
+
+export const manager = new BleManager();
+
+// Coppied from https://dotintent.github.io/react-native-ble-plx/#getting-started
+function scanAndConnect() {
+  manager.startDeviceScan(null, null, (error, device) => {
+    if (error) {
+      // Handle error (scanning will be stopped automatically)
+      return
+    }
+
+    // Check if it is a device you are looking for based on advertisement data
+    // or other criteria.
+    if (device.name === 'TI BLE Sensor Tag' || device.name === 'SensorTag') {
+      // Stop scanning as it's not necessary if you are scanning for one device.
+      manager.stopDeviceScan()
+
+      // Proceed with connection.
+    }
+  })
+}
+
